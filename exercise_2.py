4affecbfc1948f9baa09d2f348e43be02a62f015
Halil İbrahim Seyit <92215028+b2200765004@users.noreply.github.com>
2021-11-05 19:52:07 +0300

    Delete exercise_2.py

--- exercise_2.py
+++
@@ -1,9 +0,0 @@
-while True:
-    e_mail = str(input("Enter your e-mail: "))
-    x="@" in e_mail
-    y="." in e_mail
-    if(x,y)==(True,True):
-        print("Your e-mail is valid.")
-        break
-    else:
-        print("Please enter a valid e-mail.")
